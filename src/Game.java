9406b1d77cc54367d7015cb19715394ef62ad8c0
Zhejie Jiang <zhejiej@nycstudents.net>
2022-11-30 09:23:44 -0500

    adding attributes, methods and etc.

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,8 @@
 public class Game {
     private int p1money;
     private int p2money;
-    private boolean college;
+    private boolean p1college;
+    private boolean p2college;
     private int p1step;
     private int p2step;
 
@@ -23,42 +24,63 @@ public class Game {
     }
 
     //Go to college (lose $500 in the beginning but after round 5, you make $200 more than what you would make)
-    public void College (boolean college){
-        this.college = college;
+    public void p1College (boolean college){
+        this.p1college = college;
+        if(p1college){
         p1money -= 500;
-    }
-
-    public void redspace(){
-        if(p1step%5 -1 == 0){
-        System.out.println("you have stepped on to a red space, you lost $500");
         }
     }
+    public void p2College (boolean college){
+        this.p2college = college;
+        if(p2college){
+            p2money -= 500;
+        }
+    }
+    public boolean p1RedSpace(){
+        return p1step % 5 == 4;
+    }
+    public boolean p2RedSpace(){
+        return p2step % 5 == 4;
+    }
+
 
     public void beginGame(){
         int round = 1;
-        int p1step = 0;
-        int p2step = 0;
+        p1step = 0;
+        p2step = 0;
         System.out.println("Player 1 have $" + p1money );
         System.out.println("Player 2 have $" + p2money );
-        while ( (round < 11) || ((p1step > 20) && (p2step > 20)) ){
+        while ( (round < 11) || ((p1step < 20) && (p2step < 20)) ){
            System.out.println("round: " + round);
            if(p1step < 20){
                int p1DiceRoll = Dice();
                p1step += p1DiceRoll;
                System.out.println("Player 1 moved " + p1DiceRoll + " steps, Player 1 is now at step " + p1step);
 
-               if(college && p1step>5){p1money+=100;}
+               if(p1college && p1step>5 && !p1RedSpace()){p1money+=100;}
                 p1money+=200;
+                if(p1RedSpace()){
+                    System.out.println("Player 1 have stepped onto a red space, you lost $150.");
+                    p1money -= 350;
+                }
                 System.out.println("Player 1 have $" + p1money );
            }
            else{System.out.println("Player 1 have finished the game, wait");}
 
-            if(p2step < 20){
+            if(p2step < 20) {
                 int p2DiceRoll = Dice();
                 p2step += p2DiceRoll;
                 System.out.println("Player 2 moved " + p2DiceRoll + " steps, Player 2 is now at step " + p2step);
+
+                if (p2college && p2step > 5 && !p2RedSpace()) {
+                    p2money += 100;
+                }
                 p2money += 200;
-                System.out.println("Player 2 have $" + p2money );
+                if (p2RedSpace()) {
+                    System.out.println("Player 2 have stepped onto a red space, you lost $150.");
+                    p2money -= 350;
+                }
+                System.out.println("Player 2 have $" + p2money);
             }
             else{System.out.println("Player 2 have finished the game, wait");}
             round++;
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,34 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[]args){
+    public static void main(String[]args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Would you like to go to college (yes or no) ");
-        String choice = scan.nextLine();
-        System.out.print("How much money do you want to start with? (500 is the normal amount) ");
+        System.out.print("Would Player 1 like to go to college, it costs $500 (yes or no) ");
+        String choice1 = scan.nextLine();
+        System.out.print("Would Player 2 like to go to college, it costs $500 (yes or no) ");
+        String choice2 = scan.nextLine();
+        System.out.print("How much money do you guys want to start with? (500 is the normal amount) ");
         int amount = scan.nextInt();
 
 
         Game GameOfLife = new Game(amount);
-        if(choice.equals("yes")){
-            GameOfLife.College(true);
-            System.out.println("Since you chose to go to college you will lose $500, but get an extra $100 after step 5");
-        }
+        if (choice1.equals("yes") && amount >= 500) {
+            GameOfLife.p1College(true);
+            System.out.println("Since you chose to go to college you will lose $500, but get an extra $100 after step 5");}
+        else if (choice1.equals("yes") && amount < 500) {
+            System.out.println("You can't go to college, you don't have enough money to.");
+            GameOfLife.p1College(false);}
         else{
-            GameOfLife.College(false);}
-
+            GameOfLife.p1College(false);}
+        if (choice2.equals("yes") && amount >= 500) {
+            GameOfLife.p2College(true);
+            System.out.println("Since you chose to go to college you will lose $500, but get an extra $100 after step 5");}
+        else if (choice2.equals("yes") && amount < 500) {
+            System.out.println("You can't go to college, you don't have enough money to.");
+            GameOfLife.p2College(false);}
+        else{
+            GameOfLife.p2College(false);}
         //Start Game
+        System.out.println();
         GameOfLife.beginGame();
 
 
