67ab5b4bff736b9faf6f64f81132bf19ad48d417
Zhejie Jiang <zhejiej@nycstudents.net>
2022-12-02 08:30:27 -0500

    adding

--- src/Game.java
+++ src/Game.java
@@ -1,67 +1,97 @@
+/** Class to represent the game of life */
 public class Game {
+    /** Player 1's money */
     private int p1money;
-    private int p2money;
-    private boolean p1college;
-    private boolean p2college;
-    private int p1step;
-    private int p2step;
 
-    //Constructor 1, sets beginning money to $500
+    /** Player 2's money */
+    private int p2money;
+    /** player 1 goes to college? */
+    private boolean p1college;
+    /** player 2 goes to college? */
+    private boolean p2college;
+    /** what step is player 1 on */
+    private int p1step;
+    /** what step is player 2 on */
+    private int p2step;
+    /* private String winner; */
+
+    /** default constructor that sets beginning money to $500 */
     public Game(){
         this.p1money = 500;
         this.p2money = 500;
     }
 
-    //Constructor 2, sets beginning money to whatever.
+    /** Constructor that sets beginning money to whatever.
+     * @param beginningMoney - sets both player's money to this amount.
+     */
     public Game(int beginningMoney){
         this.p1money = beginningMoney;
         this.p2money = beginningMoney;
     }
 
-    //Dice roll to show how much the players move that round.
+
+    /** Dice roll to show how much the players move that round. */
     public int Dice(){
         return (int)(Math.random()*3 + 1);
     }
 
-    //Go to college (lose $500 in the beginning but after round 5, you make $200 more than what you would make)
+    /** P1 Go to college (lose $500 in the beginning but after round 5, you make $100 more)
+     * @param college - did player 1 go to college?
+     */
     public void p1College (boolean college){
         this.p1college = college;
         if(p1college){
         p1money -= 500;
         }
     }
+    /** P2 Go to college (lose $500 in the beginning but after round 5, you make $100 more)
+     * @param college - did player 2 go to college?
+     */
     public void p2College (boolean college){
         this.p2college = college;
         if(p2college){
             p2money -= 500;
         }
     }
+    /** detects if player 1 stepped on a red space */
     public boolean p1RedSpace(){
         return p1step % 5 == 4;
     }
+
+    /** detects if player 2 stepped on a red space */
     public boolean p2RedSpace(){
         return p2step % 5 == 4;
     }
 
 
+    /*
+    public String toString(){
+        if(p1money < p2money){return "The game is" + " a tie";}
+        else{return "Player " + winner + " has won the game";}
+    }
+    */
+
+    /** the game */
     public void beginGame(){
         int round = 1;
         p1step = 0;
         p2step = 0;
         System.out.println("Player 1 have $" + p1money );
         System.out.println("Player 2 have $" + p2money );
-        while ( (round < 11) || ((p1step < 20) && (p2step < 20)) ){
+        while ( (round < 11) && ((p1step < 20) && (p2step < 20)) ){
            System.out.println("round: " + round);
            if(p1step < 20){
                int p1DiceRoll = Dice();
                p1step += p1DiceRoll;
                System.out.println("Player 1 moved " + p1DiceRoll + " steps, Player 1 is now at step " + p1step);
 
-               if(p1college && p1step>5 && !p1RedSpace()){p1money+=100;}
-                p1money+=200;
-                if(p1RedSpace()){
-                    System.out.println("Player 1 have stepped onto a red space, you lost $150.");
-                    p1money -= 350;
+               if(p1college && p1step>5 && !p1RedSpace()){
+                   p1money+=100;
+               }
+               p1money+=200;
+               if(p1RedSpace()){
+                   System.out.println("Player 1 have stepped onto a red space, you lost $150.");
+                   p1money -= 350;
                 }
                 System.out.println("Player 1 have $" + p1money );
            }
@@ -86,8 +116,8 @@ public class Game {
             round++;
             System.out.println();
         }
-        if(p1money > p2money){System.out.println("Player 1 has won the game");}
-        else if(p1money < p2money){System.out.println("Player 2 has won the game");}
-        else{System.out.println("The game is a tie");}
+        if(p1money > p2money){System.out.println("Player "+ 1 +" has won the game");}
+        else if(p1money < p2money){System.out.println("Player " + 2 + " has won the game");}
+        else{System.out.println("The game is" + " a tie");}
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
+
+/** Class that runs the game */
 public class Main {
     public static void main(String[]args) {
         Scanner scan = new Scanner(System.in);
+        //intro
+        System.out.println("Welcome to a worse version of \" The Game Of Life\"");
+
+        //college for player 1?
         System.out.print("Would Player 1 like to go to college, it costs $500 (yes or no) ");
         String choice1 = scan.nextLine();
+        //college for player 2?
         System.out.print("Would Player 2 like to go to college, it costs $500 (yes or no) ");
         String choice2 = scan.nextLine();
+        //Starting money
         System.out.print("How much money do you guys want to start with? (500 is the normal amount) ");
         int amount = scan.nextInt();
 
-
+        //Makes the game
         Game GameOfLife = new Game(amount);
+
+        //Checks if Player 1 can go to college
         if (choice1.equals("yes") && amount >= 500) {
             GameOfLife.p1College(true);
             System.out.println("Since you chose to go to college you will lose $500, but get an extra $100 after step 5");}
@@ -19,6 +29,8 @@ public class Main {
             GameOfLife.p1College(false);}
         else{
             GameOfLife.p1College(false);}
+
+        //Checks if Player 2 can go to college
         if (choice2.equals("yes") && amount >= 500) {
             GameOfLife.p2College(true);
             System.out.println("Since you chose to go to college you will lose $500, but get an extra $100 after step 5");}
@@ -27,6 +39,7 @@ public class Main {
             GameOfLife.p2College(false);}
         else{
             GameOfLife.p2College(false);}
+
         //Start Game
         System.out.println();
         GameOfLife.beginGame();
